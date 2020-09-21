6a3fe8684e5965d725d3fa6b71d2f8e493096f49
peppe965 <giuseppe.falco000@gmail.com>
2020-09-21 16:10:40 +0200

    now it's possible to change month in every moment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Creiamo un calendario dinamico con le festività.
-// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018
-// (unici dati disponibili sull’API).
-// Milestone 1
-// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
-// API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+// // Creiamo un calendario dinamico con le festività.
+// // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018
+// // (unici dati disponibili sull’API).
+// // Milestone 1
+// // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+// // API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
 
 $(document).ready(function(){
     
@@ -20,94 +20,133 @@ $(document).ready(function(){
         actualTime();
     }, 30000);
     
-
-    var day = 12;
-    var month = prompt("mese");
-    if (month <10) {
-        month = "0" + month;
-    }
+    //assegnazione inizio calendario
     var year = 2018;
-    var date = year + "-" + month + "-" + day;
-    console.log(date);
-    var momentDate = moment(date);
-    console.log(momentDate);
-    var dayInMonth = momentDate.daysInMonth();
-    
-    //stampa mese in pagina
-    $("header h1").text(momentDate.format("MMMM"))
-    //stampa giorni in pagina    
-    for (var i=1; i<=dayInMonth; i++) {
+    var month = 1;
 
-        if (i<10) {
-            i = "0" + i;
-        }
-        var context = {
-            "day": i,
-            "month": momentDate.format("MMMM"),
-            "date-complete":momentDate.format("YYYY-MM-" + i)
-        }
-        var html = template(context);
-                    
-        $(".month-wrapper").append(html);
+  
 
-    }
+    renderPage();
 
-    $.ajax(
-        {
-            url: "https://flynn.boolean.careers/exercises/api/holidays",
-            data:{
-                "year": year,
-                "month": parseInt(month) - 1,
-            },
-            method: "GET",
-            success: function (data) {
-                console.log(data);
-                
-                var results = data.response;
-                console.log(results);
 
-                if (results.length > 0) {
-                    for( var i=0; i<results.length; i++) {
-
-                        var holidaysName = results[i].name;
-                        var holidaysDate = results[i].date;
-
-                        $(".day[data-date='"+holidaysDate+"']").addClass("holiday");
-                        var test = $(".day[data-date='"+holidaysDate+"']").text();
-                        $(".day[data-date='"+holidaysDate+"']").text(test + " - " + holidaysName);
-                    }
-                }
+    // Definisco l'evento click sul pulsante PREV
+    $("#prev").click(function() {
+      // che funziona solo se startMonth è maggiore di 0
+      if (month > 1) {
+        // Seleziono il mese precedente
+        month -= 1;
+        // Svuoto la pagina
+        $(".month-wrapper").empty();
+        // Effettuo nuovamente il render
+        renderPage();
+      } else {
+        // Definisco un alert nel caso l'utente prema il pulsante PREV su Gennaio 2018
+        alert("Nessun dato precedente a questa data!");
+      }
+    });
+  
+    // Definisco l'evento click sul pulsante NEXT
+    $("#next").click(function() {
+      // che funziona solo se startMonth è minore di 11
+      if (month < 12) {
+        // Seleziono il mese successivo
+        month += 1;
+        console.log(month);
+        
+        // Svuoto la pagina
+        $(".month-wrapper").empty();
+        // Effettuo nuovamente il render
+        renderPage();
+      } else {
+        // Definisco un alert nel caso l'utente prema il pulsante NEXT su Dicembre 2018
+        alert("Nessun dato successivo a questa data!");
+      }
+    });
 
 
 
-                 
-              
-
-
-
-               
-
-
-            },
-            error: function () {
-                alert("Imprevisto, riprova !")
-            }
-        }
-    )
-
-
-
-
-})
 
 
 //*************************************FUNZIONI */
     //ritorna orario e data attuale in alto a destra in pagina
     function actualTime() {
         var now = moment();
-        var formattedTime = now.format("hh:mm");
+        var formattedTime = now.format("HH:mm ");
         var formattedDate = now.format("dddd, DD MMMM YYYY") ;
 
         $("#time-actual").text(formattedTime);
         $("#day-actual").text(formattedDate);
+    };
+
+    function renderPage() {
+      console.log(month);
+      
+        // Effettuo la chiamata Ajax verso l'api contenente le festività dell'anno 2018
+        $.ajax(
+          {
+            "url": "https://flynn.boolean.careers/exercises/api/holidays",
+            "data": {
+              "year": year,
+              "month": month - 1
+            },
+            "method": "GET",
+            "success": function(data) {
+              console.log(data.response);
+              
+              printHolidays(data.response);
+            },
+            "error": function (error) {
+              alert("Errore! Riprova");
+            }
+          }
+        );
+      }
+    //ritorna giorni con festività
+    function printHolidays(holidays) {
+
+      var date = {
+        "year": year,
+        "month": month - 1
+      }
+
+      var momentDate = moment(date);
+      
+      var dayInMonth = momentDate.daysInMonth();
+      
+        //stampa mese in pagina
+        $("header h1").text(momentDate.format("MMMM"))
+        //stampa giorni in pagina    
+        for (var i=1; i<=dayInMonth; i++) {
+
+            if (i<10) {
+                i = "0" + i;
+            }
+            var context = {
+                "day": i,
+                "month": momentDate.format("MMMM"),
+                "date-complete":momentDate.format("YYYY-MM-" + i)
+            }
+            var html = template(context);
+                        
+            $(".month-wrapper").append(html);
+
+        }
+
+        
+        if (holidays.length > 0) {
+            for( var i=0; i<holidays.length; i++) {
+
+                var holidaysName = holidays[i].name;
+                var holidaysDate = holidays[i].date;
+
+                $(".day[data-date='"+holidaysDate+"']").addClass("holiday");
+                var test = $(".day[data-date='"+holidaysDate+"']").text();
+                $(".day[data-date='"+holidaysDate+"']").text(test + " - " + holidaysName);
+            }
+        }
+
     }
+    
+
+  });
+
